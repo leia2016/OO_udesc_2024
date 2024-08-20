1ac633d1303f46d582022b7e1f27438d21295c5d
Marileia <marileia.direito@gmail.com>
2024-08-19 21:48:28 -0300

    classe  OlaMundo e respectivo teste

---
+++ src/OlaMundo.java
@@ -0,0 +1,13 @@
+public class OlaMundo {
+    String nome="Udesc!";
+    int idade =59;
+
+    void imprimirDados(){
+
+        System.out.println (nome +"-"+ idade);
+
+    }
+    void imprimir(String nome,int idade){
+        System.out.println (nome +"-"+ idade);
+    }
+}
---
+++ src/TestaOlaMundo.java
@@ -0,0 +1,8 @@
+public class TestaOlaMundo {
+    public static void main (String[]args){
+        OlaMundo ola = new OlaMundo();
+        ola.imprimirDados();
+        ola.imprimir("Fulano",47);
+
+    }
+}
